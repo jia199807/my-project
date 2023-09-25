13287d75cde5a87aa8aa85fe575f37f606ba333e
jty <642096532@qq.com>
2023-09-25 23:02:32 +0800

    配置SpringSecurity基本内容

---
+++ my-project-backend/src/main/java/com/example/entity/RestBean.java
@@ -0,0 +1,28 @@
+package com.example.entity;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+
+/**
+ * @program: my-project
+ * @description: 响应实体类
+ * @author: 6420
+ * @create: 2023-09-25 22:45
+ **/
+public record RestBean<T>(int code,T data,String message) {
+
+    public static <T> RestBean<T> success(T data){
+        return new RestBean<>(200,data,"请求成功");
+    }
+    public static <T> RestBean<T> success(){
+        return success(null);
+    }
+
+    public String asJsonString(){
+        return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);
+    }
+
+    public static <T> RestBean<T> failure(int code,String message){
+        return new RestBean<>(code,null,message);
+    }
+}
