10a1423d8eaef82859383531e5164cd6537f4723
jty <642096532@qq.com>
2023-10-05 19:09:38 +0800

    跨域手动配置

--- my-project-backend/src/main/java/com/example/filter/CorsFilter.java
+++ my-project-backend/src/main/java/com/example/filter/CorsFilter.java
@@ -1,7 +1,15 @@
 package com.example.filter;
 
+import com.example.utils.Const;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @program: my-project
@@ -10,6 +18,17 @@ import org.springframework.stereotype.Component;
  * @create: 2023-09-29 19:12
  **/
 @Component
-@Order()
-public class CorsFilter {
+@Order(Const.ORDER_CORS)
+public class CorsFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        this.addCorsHeader(request,response);
+        chain.doFilter(request,response);
+    }
+    private void addCorsHeader(HttpServletRequest request, HttpServletResponse response){
+        response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.addHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+    }
 }
